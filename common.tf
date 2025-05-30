e92e9bec07d57672f194aff0417b5c95542840a1
Gavin Jackson <gavinjny@gmail.com>
2025-05-30 09:59:34 -0400

    test

--- common.tf
+++ common.tf
@@ -2,7 +2,7 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
 variable "security_group" {}
-variable "iam_instance_profile" {}
+# variable "iam_instance_profile" {}
 variable "aws_region" {}
 variable "vpc" {}
 variable "subnet" {}
