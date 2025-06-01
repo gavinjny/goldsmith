b19209c046dd944d245adf0a5ae85dad264a1abc
Gavin Jackson <gavinjny@gmail.com>
2025-06-01 15:38:07 -0400

    test

--- alb-web.tf
+++ alb-web.tf
@@ -4,7 +4,6 @@ resource "aws_lb" "app_alb" {
   load_balancer_type = "application"
   security_groups    = var.security_group
   subnets            = var.aws_vpc_zone_identifier
-#   subnets = ["subnet-391cc04e", "subnet-8fd72ad6", "subnet-54ad737f", "subnet-b65fe1d3"]
 }
 
 resource "aws_lb_target_group" "v1" {
--- asg-web.tf
+++ asg-web.tf
@@ -4,8 +4,6 @@ resource "aws_autoscaling_group" "demo_asg" {
   max_size             = 3
   min_size             = 1
   vpc_zone_identifier  = var.aws_vpc_zone_identifier
-  # vpc_zone_identifier = ["subnet-391cc04e", "subnet-8fd72ad6", "subnet-54ad737f", "subnet-b65fe1d3"]
-
 
   launch_template {
     id      = aws_launch_template.ec2_template.id
@@ -21,4 +19,15 @@ resource "aws_autoscaling_group" "demo_asg" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_instance_refresh" "refresh" {
+  autoscaling_group_name = aws_autoscaling_group.demo_asg.name
+
+  preferences {
+    min_healthy_percentage = 100
+    instance_warmup        = 300
+  }
+
+  triggers = ["launch_template"]
 }
