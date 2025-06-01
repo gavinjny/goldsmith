da7479b6c260cca151da3cd77fb96e251c3dbc21
Gavin Jackson <gavinjny@gmail.com>
2025-06-01 18:10:03 -0400

    test

--- versions.tf
+++ versions.tf
@@ -2,7 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.6.0" # Or a newer specific version like "~> 5.0"
+      version = ">= 4.37.0"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
