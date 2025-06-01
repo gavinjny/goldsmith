81af9002e9848dad11ae34c766d96bc96d1b4a8b
Gavin Jackson <gavinjny@gmail.com>
2025-06-01 16:15:11 -0400

    test

--- asg-web.tf
+++ asg-web.tf
@@ -1,23 +1,3 @@
-resource "aws_launch_template" "ec2_template" {
-  name_prefix   = "lt-web-demo-"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = var.security_group
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = {
-      Name = "lt-web-demo"
-    }
-  }
-}
-
 resource "aws_autoscaling_group" "demo_asg" {
   name                 = "asg-web-demo"
   desired_capacity     = 2
