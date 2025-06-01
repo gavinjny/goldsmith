76cf7050a48674b26002a3f850e0caeff9b331c3
Gavin Jackson <gavinjny@gmail.com>
2025-06-01 13:44:58 -0400

    test

--- asg-web.tf
+++ asg-web.tf
@@ -3,8 +3,8 @@ resource "aws_autoscaling_group" "demo_asg" {
   desired_capacity     = 2
   max_size             = 3
   min_size             = 1
-  # vpc_zone_identifier  = var.aws_vpc_zone_identifier
-  vpc_zone_identifier = ["subnet-391cc04e", "subnet-8fd72ad6", "subnet-54ad737f", "subnet-b65fe1d3"]
+  vpc_zone_identifier  = var.aws_vpc_zone_identifier
+  # vpc_zone_identifier = ["subnet-391cc04e", "subnet-8fd72ad6", "subnet-54ad737f", "subnet-b65fe1d3"]
 
 
   launch_template {
