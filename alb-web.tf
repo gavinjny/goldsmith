59755496d7697c0a1250d8eed293af274214baca
Gavin Jackson <gavinjny@gmail.com>
2025-06-02 01:28:48 -0400

    test

--- alb-web.tf
+++ alb-web.tf
@@ -7,14 +7,14 @@ resource "aws_lb" "app_alb" {
 }
 
 resource "aws_lb_target_group" "v1" {
-  name     = "tg-v1"
+  name     = "v1"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc
 }
 
 resource "aws_lb_target_group" "v2" {
-  name     = "tg-v2"
+  name     = "v2"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc
@@ -34,6 +34,7 @@ resource "aws_lb_target_group_attachment" "tg_v2_attach" {
 }
 
 resource "aws_lb_listener" "http" {
+
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
