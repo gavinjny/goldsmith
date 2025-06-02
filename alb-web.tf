ad53c7adedd6b88e20c9cf6d4af4741483a687fc
Gavin Jackson <gavinjny@gmail.com>
2025-06-02 01:16:53 -0400

    test

--- alb-web.tf
+++ alb-web.tf
@@ -19,6 +19,19 @@ resource "aws_lb_target_group" "v2" {
   protocol = "HTTP"
   vpc_id   = var.vpc
 }
+resource "aws_lb_target_group_attachment" "tg_v1_attach" {
+  count            = 1
+  target_group_arn = aws_lb_target_group.v1.arn
+  target_id        = aws_instance.web[0].id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg_v2_attach" {
+  count            = 1
+  target_group_arn = aws_lb_target_group.v2.arn
+  target_id        = aws_instance.web[1].id
+  port             = 80
+}
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
