13bac73d44f97142c7d22c1149606a8c3f4c8f5d
Gavin Jackson <gavinjny@gmail.com>
2025-06-02 10:01:38 -0400

    test

--- alb-web.tf
+++ alb-web.tf
@@ -8,11 +8,11 @@ resource "aws_lb_listener" "http" {
     forward {
       target_group {
         arn    = aws_lb_target_group.v1.arn
-        weight = 90
+        weight = 50
       }
       target_group {
         arn    = aws_lb_target_group.v2.arn
-        weight = 10
+        weight = 50
       }
       stickiness {
         enabled  = false
