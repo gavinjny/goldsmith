ebb2c46603a2c9aaaf95f4b4443c66d9133d0153
Gavin Jackson <gavinjny@gmail.com>
2025-06-02 08:48:07 -0400

    test

--- alb-web.tf
+++ alb-web.tf
@@ -1,43 +1,8 @@
-resource "aws_lb" "app_alb" {
-  name               = "alb-web-demo"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = var.security_group
-  subnets            = var.aws_vpc_zone_identifier
-}
-
-resource "aws_lb_target_group" "v1" {
-  name     = "v1"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc
-}
-
-resource "aws_lb_target_group" "v2" {
-  name     = "v2"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc
-}
-resource "aws_lb_target_group_attachment" "tg_v1_attach" {
-  count            = 1
-  target_group_arn = aws_lb_target_group.v1.arn
-  target_id        = aws_instance.web[0].id
-  port             = 80
-}
-
-resource "aws_lb_target_group_attachment" "tg_v2_attach" {
-  count            = 1
-  target_group_arn = aws_lb_target_group.v2.arn
-  target_id        = aws_instance.web[1].id
-  port             = 80
-}
-
 resource "aws_lb_listener" "http" {
-
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type = "forward"
     forward {
--- asg-web.tf
+++ asg-web.tf
@@ -1,18 +1,89 @@
-resource "aws_autoscaling_group" "demo_asg" {
-  name                 = "asg-web-demo"
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 1
-  vpc_zone_identifier  = var.aws_vpc_zone_identifier
+resource "aws_launch_template" "ec2_template" {
+  name_prefix   = "lt-web-demo-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
-launch_template {
-  id      = aws_launch_template.ec2_template.id
-  version = "$Latest"
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = var.security_group
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "lt-web-demo"
+    }
+  }
 }
+
+resource "aws_lb" "app_alb" {
+  name               = "alb-web-demo"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group
+  subnets            = var.aws_vpc_zone_identifier
+}
+
+# Target Groups
+resource "aws_lb_target_group" "v1" {
+  name     = "v1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc
+}
+
+resource "aws_lb_target_group" "v2" {
+  name     = "v2"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc
+}
+
+# Auto Scaling Group - Stable (v1)
+resource "aws_autoscaling_group" "asg_v1" {
+  name                = "asg-web-demo-v1"
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  vpc_zone_identifier = var.aws_vpc_zone_identifier
+
+  target_group_arns = [aws_lb_target_group.v1.arn]
+
+  launch_template {
+    id      = aws_launch_template.ec2_template.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
-    value               = "ec2-web-demo"
+    value               = "ec2-web-demo-v1"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Auto Scaling Group - Canary (v2)
+resource "aws_autoscaling_group" "asg_v2" {
+  name                = "asg-web-demo-v2"
+  desired_capacity    = 1
+  max_size            = 2
+  min_size            = 1
+  vpc_zone_identifier = var.aws_vpc_zone_identifier
+
+  target_group_arns = [aws_lb_target_group.v2.arn]
+
+  launch_template {
+    id      = aws_launch_template.ec2_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ec2-web-demo-v2"
     propagate_at_launch = true
   }
 
