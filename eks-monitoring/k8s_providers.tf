1776cb6f352bd46ff74935a271fe26e4d6a042a0
Gavin Jackson <gavinjny@gmail.com>
2025-10-08 21:57:42 -0400

    test

--- eks-monitoring/k8s_providers.tf
+++ eks-monitoring/k8s_providers.tf
@@ -18,3 +18,26 @@ provider "helm" {
     load_config_file       = false
   }
 }
+
+
+
+----------
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+    load_config_file       = false
+  }
+}
