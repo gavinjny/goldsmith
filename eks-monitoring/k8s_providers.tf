08657f9af4b35d88f40396f6921de6fe241b152f
Gavin Jackson <gavinjny@gmail.com>
2025-10-08 23:54:05 -0400

    test

--- eks-monitoring/k8s_providers.tf
+++ eks-monitoring/k8s_providers.tf
@@ -1,6 +1,5 @@
 data "aws_eks_cluster_auth" "this" {
-  provider = aws.k8s
-  name     = module.eks.cluster_name
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
@@ -9,12 +8,10 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-# Use attribute style for broad provider compatibility
 provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
-    load_config_file       = false
   }
 }
--- eks-monitoring/providers.tf
+++ eks-monitoring/providers.tf
@@ -1,11 +1,11 @@
 # Uses AWS creds/region from your GitHub Actions secrets
 provider "aws" {}
 
-provider "aws" {
-  alias = "k8s"
+# provider "aws" {
+#   alias = "k8s"
 
-  assume_role {
-    role_arn     = var.pipeline_role_arn
-    session_name = "tf-k8s"
-  }
-}
+#   assume_role {
+#     role_arn     = var.pipeline_role_arn
+#     session_name = "tf-k8s"
+#   }
+# }
