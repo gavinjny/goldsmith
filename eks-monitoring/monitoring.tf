2229a3f5b72d946f06a0f41e872f86b84312faab
Gavin Jackson <gavinjny@gmail.com>
2025-10-08 21:29:38 -0400

    test

--- eks-monitoring/monitoring.tf
+++ eks-monitoring/monitoring.tf
@@ -12,13 +12,17 @@ resource "helm_release" "kps" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  # version  = "65.5.0" # optional pin
 
-  # Minimal demo-friendly values
-  set { name = "grafana.adminUser",             value = "admin" }
-  set { name = "grafana.adminPassword",         value = random_password.grafana.result }
-  set { name = "grafana.service.type",          value = "LoadBalancer" }
-  set { name = "grafana.persistence.enabled",   value = "false" } # ephemeral for demo
+  values = [
+    yamlencode({
+      grafana = {
+        adminUser     = "admin"
+        adminPassword = random_password.grafana.result
+        service       = { type = "LoadBalancer" }
+        persistence   = { enabled = false }
+      }
+    })
+  ]
 
   depends_on = [module.eks]
 }
